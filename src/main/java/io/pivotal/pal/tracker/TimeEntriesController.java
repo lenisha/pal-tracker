8da10576ee412fb315dd0840ae518d775dcba04e
Elena Neroslavskaya <eneroslavskaya@pivotal.io>
2017-07-11 14:59:30 -0400

    add jdbc

--- src/main/java/io/pivotal/pal/tracker/TimeEntriesController.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntriesController.java
@@ -14,51 +14,51 @@ import java.util.List;
 @RequestMapping("/timeEntries")
 public class TimeEntriesController {
 
-        private TimeEntryRepository timeEntriesRepo;
+    private TimeEntryRepository timeEntriesRepo;
 
-        public TimeEntriesController(TimeEntryRepository timeEntriesRepo) {
-            this.timeEntriesRepo = timeEntriesRepo;
-        }
+    public TimeEntriesController(TimeEntryRepository timeEntriesRepo) {
+        this.timeEntriesRepo = timeEntriesRepo;
+    }
 
-        @PostMapping
-        public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry) {
-            TimeEntry createdTimeEntry = timeEntriesRepo.create(timeEntry);
+    @PostMapping
+    public ResponseEntity<TimeEntry> create(@RequestBody TimeEntry timeEntry) {
+        TimeEntry createdTimeEntry = timeEntriesRepo.create(timeEntry);
 
-            return new ResponseEntity<>(createdTimeEntry, HttpStatus.CREATED);
-        }
+        return new ResponseEntity<>(createdTimeEntry, HttpStatus.CREATED);
+    }
 
-        @GetMapping("{id}")
-        @ResponseBody
-        public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
-            TimeEntry timeEntry = timeEntriesRepo.get(id);
-            if (timeEntry != null) {
-                return new ResponseEntity<>(timeEntry, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        }
-
-        @GetMapping
-        public ResponseEntity<List<TimeEntry>> list() {
-            return new ResponseEntity<>(timeEntriesRepo.list(), HttpStatus.OK);
-        }
-
-        @PutMapping("{id}")
-        public ResponseEntity<TimeEntry> update(@PathVariable Long id, @RequestBody TimeEntry timeEntry) {
-            TimeEntry updatedTimeEntry = timeEntriesRepo.update(id, timeEntry);
-            if (updatedTimeEntry != null) {
-                return new ResponseEntity<>(updatedTimeEntry, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        }
-
-        @DeleteMapping("{id}")
-        public ResponseEntity<TimeEntry> delete(@PathVariable Long id) {
-            timeEntriesRepo.delete(id);
-
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    @GetMapping("{id}")
+    @ResponseBody
+    public ResponseEntity<TimeEntry> read(@PathVariable Long id) {
+        TimeEntry timeEntry = timeEntriesRepo.get(id);
+        if (timeEntry != null) {
+            return new ResponseEntity<>(timeEntry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list() {
+        return new ResponseEntity<>(timeEntriesRepo.list(), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<TimeEntry> update(@PathVariable Long id, @RequestBody TimeEntry timeEntry) {
+        TimeEntry updatedTimeEntry = timeEntriesRepo.update(id, timeEntry);
+        if (updatedTimeEntry != null) {
+            return new ResponseEntity<>(updatedTimeEntry, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<TimeEntry> delete(@PathVariable Long id) {
+        timeEntriesRepo.delete(id);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
 
 
--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -8,8 +8,12 @@ import java.util.List;
  */
 public interface TimeEntryRepository {
     TimeEntry create(TimeEntry timeEntry);
+
     TimeEntry get(Long id);
+
     List<TimeEntry> list();
+
     TimeEntry update(Long id, TimeEntry timeEntry);
+
     void delete(Long id);
 }
